a4d98e6b3e425143e6172cc5a55f54ef28b3c902
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-05-25 21:36:43 +0200

    adds App routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
+import SharedLayout from "components/SharedLayout";
+import Home from "pages/Home";
+import Movies from "pages/Movies";
+import MovieCard from "pages/MovieCard";
 
 export const App = () => {
   return (
     <BrowserRouter basename="/goit-react-hw-05-movies">
     <div>
       <Routes>
-        <Route path="/">
-
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />}/>
+          <Route path="movies" element={<Movies />}/>
+          <Route path="/movies/:movieId" element={<MovieCard />}/>
         </Route>
       </Routes>
     </div>
