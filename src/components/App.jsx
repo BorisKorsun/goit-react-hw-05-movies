eca5b46e7a632c03a6297e7789cc2b6b52619034
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-05-25 21:17:33 +0200

    components and pages init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 
 export const App = () => {
   return (
     <BrowserRouter basename="/goit-react-hw-05-movies">
     <div>
-      React homework template
+      <Routes>
+        <Route path="/">
+
+        </Route>
+      </Routes>
     </div>
     </BrowserRouter>
   );
