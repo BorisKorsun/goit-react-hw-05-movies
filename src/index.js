3dc94e43d2a10ee68fa1b30a475d3a96aa28d234
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-05-27 23:44:04 +0200

    build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import { Global } from '@emotion/react';
 import { GlobalStyles } from 'styles/globalStyles';
+import { Global } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
 
 
